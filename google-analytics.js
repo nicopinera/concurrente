f55e692acc19b72e835339756ccf9d2118337799
Nicolas Piñera <nicolaspinera@gmail.com>
2025-12-07 21:07:25 -0300

    Deployed e195446 with MkDocs version: 1.6.1

--- google-analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-S6ECQ3P6X4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-S6ECQ3P6X4');
-</script>
